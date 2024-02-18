727262a249da1ee351e7cd2d45d502d857b12b86
Michael Baldwin <Michael.Baldwin05@gmail.com>
2024-02-17 22:10:36 -0800

    Update AwaitCondition.java

--- BotWithUsExampleScript-master/src/main/java/net/botwithus/debug/AwaitCondition.java
+++ BotWithUsExampleScript-master/src/main/java/net/botwithus/debug/AwaitCondition.java
@@ -1,5 +1,5 @@
 package net.botwithus.debug;
-import java.util.concurrent.CompletableFuture;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Future;
@@ -7,35 +7,35 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 import net.botwithus.rs3.script.ScriptConsole;
-import net.botwithus.rs3.util.RandomGenerator;
-import net.botwithus.rs3.script.Script;
-//////Is there a better way to await a condition being met? Please help me Cipher.
 public class AwaitCondition {
 
- public static void await(Supplier<Boolean> condition, long timeout, TimeUnit unit) throws InterruptedException {
-     ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-     try {
-         Future<?> future = executor.scheduleAtFixedRate(() -> {
-             if (condition.get()) {
-                 throw new RuntimeException("Condition met");
-             }
-         }, 0, 1100, TimeUnit.MILLISECONDS);
+    public static void await(Supplier<Boolean> condition, long timeout, TimeUnit unit) throws InterruptedException {
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        long startTime = System.currentTimeMillis();
+        long delay = unit.toMillis(timeout);
 
-         executor.schedule(() -> {
-             future.cancel(true);
-         }, timeout, unit);
+        try {
+            Future<?> future = executor.scheduleAtFixedRate(() -> {
+                if (System.currentTimeMillis() - startTime < delay && condition.get()) {
+                    throw new RuntimeException("Condition met");
+                }
+            }, 0, 590, TimeUnit.MILLISECONDS);
 
-         while (!future.isDone()) {
-             Thread.sleep(1100);
-         }
-     } catch (RuntimeException e) {
-         if (!e.getMessage().equals("Condition met")) {
-             throw e;
-         }
-     } catch(Exception e){
-         ScriptConsole.println(e.getMessage());
-     } finally {
-         executor.shutdownNow();
-     }
- }
+            executor.schedule(() -> {
+                future.cancel(true);
+            }, timeout, unit);
+
+            while (!future.isDone()) {
+                Thread.sleep(590);
+            }
+        } catch (RuntimeException e) {
+            if (!e.getMessage().equals("Condition met")) {
+                throw e;
+            }
+        } catch(Exception e){
+            ScriptConsole.println(e.getMessage());
+        } finally {
+            executor.shutdownNow();
+        }
+    }
 }
